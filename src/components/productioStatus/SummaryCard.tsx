df8e603f1d6af8230c55bf823e2fdcc159bcd782
nizam <nizampnizam7@gmail.com>
2025-09-03 12:30:18 +0530

    enhanced ui to be exacted

--- src/components/productioStatus/SummaryCard.tsx
+++ src/components/productioStatus/SummaryCard.tsx
@@ -3,21 +3,13 @@ import React from "react";
 interface SummaryCardProps {
   title: string;
   value: number;
-  subtitle: string;
-  icon: React.ReactNode;
 }
 
-const SummaryCard: React.FC<SummaryCardProps> = ({ title, value, subtitle, icon }) => {
+const SummaryCard: React.FC<SummaryCardProps> = ({ title, value }) => {
   return (
-    <div className="relative flex flex-col p-4 h-40">
-      <h2 className="text-gray-700 font-sm mb-6">{title}</h2>
-
-      <div className="text-5xl text-gray-700  mt-2">{value}</div>
-
-      <div className="absolute bottom-0 left-4 flex items-center text-gray-600 mt-2 text-sm">
-        {subtitle}
-        <span className="ml-2">{icon}</span>
-      </div>
+    <div className="relative flex flex-col p-4">
+      <h2 className="text-gray-700 text-sm mb-6">{title}</h2>
+      <div className="text-5xl text-gray-700 mt-2">{value}</div>
     </div>
   );
 };
--- src/views/ProductionStatusDashboard.tsx
+++ src/views/ProductionStatusDashboard.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useAttendanceViewModel } from "../viewmodels/AttendanceViewModel";
 import { useLeaveViewModel } from "../viewmodels/LeaveViewModel";
-import { Users, Users2 } from "lucide-react";
+import { Users2 } from "lucide-react";
 import SummaryCard from "../components/productioStatus/SummaryCard";
 import Chart from "../components/productioStatus/Chart";
 
@@ -15,34 +15,46 @@ const ProductioStatus: React.FC = () => {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-6 p-6 bg-gray-100">
-      {/* Attendance Section */}
+      {/* Attendance  */}
       <div className="bg-white rounded-2xl shadow p-4">
         <SummaryCard
           title="Attendance"
           value={attendance.overall}
-          subtitle="Overall Attendance"
-          icon={<Users className="w-4 h-4 text-blue-500" />}
         />
-        <Chart
-          data={attendance.chartData}
-          color="#3b82f6"
-          rateTitle="Attendance Rate"
-        />
+
+        <div className="flex justify-between items-center px-4 mb-8 text-sm text-gray-600">
+          <div className="flex items-center">
+            Overall Attendance
+            <Users2 className="w-4 h-4 text-blue-500 ml-2" />
+          </div>
+          <div className="flex items-center">
+            <div className="w-4 h-3 rounded-sm mr-2" style={{ backgroundColor: "#00A0E3" }}></div>
+            Attendance Rate
+          </div>
+        </div>
+
+        <Chart data={attendance.chartData} color="#00A0E3" />
       </div>
 
-      {/* Leave Section */}
+      {/* Leave  */}
       <div className="bg-white rounded-2xl shadow p-4">
         <SummaryCard
           title="Leave"
           value={leaves.overall}
-          subtitle="Overall Leaves"
-          icon={<Users2 className="w-4 h-4 text-orange-500" />}
         />
-        <Chart
-          data={leaves.chartData}
-          color="#f97316"
-          rateTitle="Leave Rate"
-        />
+
+        <div className="flex justify-between items-center px-4 mb-2 text-sm text-gray-600">
+          <div className="flex items-center">
+            Overall Leaves
+            <Users2 className="w-4 h-4 text-orange-500 ml-2" />
+          </div>
+          <div className="flex items-center">
+            <div className="w-4 h-3 rounded-sm mr-1" style={{ backgroundColor: "#FD7F20" }}></div>
+            Leave Rate
+          </div>
+        </div>
+
+        <Chart data={leaves.chartData} color="#FD7F20" />
       </div>
     </div>
   );
